d839b292eab6f2f33d192cfe1b35c8fdba13e717
danuf <33052960+danuf@users.noreply.github.com>
2018-08-07 18:46:14 +0300

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -2,12 +2,6 @@ pkg_name=simple-jar
 pkg_origin=danuf
 pkg_version="0.0.1"
 pkg_deps=(core/jdk8/8u131 core/jre8 core/maven)
-pkg_build_deps=(core/jdk8/8u131 core/jre8 core/maven)
-
-do_prepare()
-{
-    export JAVA_HOME=$(hab pkg path core/jdk8)
-}
 
 do_build() {
   mvn clean install -DskipTests=true
