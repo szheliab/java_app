14ebfcd51a6d2961d2bee231923df4d1a983fff7
Dmytro Anufriiev <33052960+danuf@users.noreply.github.com>
2019-01-29 16:37:42 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 JAVA_ARGS=""
 
-JAVA_ARGS="${JAVA_ARGS} ${JAVA_OPTS} -Dserver.port=8080 -jar ${JAR_DIR}${JAR_NAME}"
+JAVA_ARGS="${JAVA_ARGS} ${JAVA_OPTS} -Dserver.port=${JAVA_PORT} -jar ${JAR_DIR}${JAR_NAME}"
 
 echo "> Executing: java ${JAVA_ARGS} "
 java ${JAVA_ARGS}
